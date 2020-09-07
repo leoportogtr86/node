edf12d721de6f1581034240a5a0e924041888589
leoporto <leonardoreisporto@gmail.com>
2020-09-07 08:22:57 -0300

    modulos em node

---
+++ modulos/app.js
@@ -0,0 +1,11 @@
+const calculadora = require('./calculadora')
+
+//para acessar cada funcao do modulo "calculadora" usamos o ponto (.)
+
+console.log(calculadora.soma(10, 2))
+
+console.log(calculadora.soma(9, 80))
+
+console.log(calculadora.divisao(8, 3))
+
+console.log(calculadora.subtracao(9, 5))
---
+++ modulos/calculadora.js
@@ -0,0 +1,30 @@
+function soma(a, b) {
+
+    return a + b
+    
+}
+
+function divisao(a, b) {
+
+    return a/b
+    
+}
+
+function multiplicacao(a, b) {
+
+    return a * b
+    
+}
+
+function subtracao(a, b) {
+
+    return a - b
+    
+}
+
+
+
+//exportando multiplos arquivos via objeto
+
+
+module.exports = {divisao, soma, multiplicacao, subtracao}
