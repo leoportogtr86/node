b63f21599165f1a6c6f7ac33ce9f3a7c3ecd5345
Leo Porto <leoporto_studio@hotmail.com>
2020-09-24 14:05:05 -0300

    abrindo a conexão entre o frontend e backend

--- socket-io/app.js
+++ socket-io/app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+
+io.on('connection', socket => {
+
+    console.log(socket)
+    console.log(socket.id)
+})
+
+
+
 
 
 
