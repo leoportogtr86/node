4d8cf030db153381898ed12b717db9a072cd9987
Leo Porto <leonardoreisporto@gmail.com>
2020-10-07 06:11:34 -0300

    configurando a conexao com o banco de dados

---
+++ projeto-perguntas-respostas/database/database.js
@@ -0,0 +1,10 @@
+const sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
+
+
+//nome do banco, user e senha
+const con = new Sequelize('guia_perguntas', 'root', '151187', {
+
+    host: 'localhost',
+    dialect: 'mysql'
+})
