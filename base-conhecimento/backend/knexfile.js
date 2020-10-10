2ac19a41c8fcce5f02791d9f57b2c357849b8429
Leo Porto <leonardoreisporto@gmail.com>
2020-10-10 07:43:53 -0300

    criando o arquivo de configuacao do knex que conectara com o banco de dados => knex init

--- base-conhecimento/backend/knexfile.js
+++ base-conhecimento/backend/knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+//knex init
 module.exports = {
 
   development: {
@@ -13,7 +13,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -29,7 +29,7 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
