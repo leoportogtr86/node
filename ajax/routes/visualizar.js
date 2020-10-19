4118d330dc30f57bf714cf5dd918e25955b981fb
Leo Porto <leonardoreisporto@gmail.com>
2020-10-18 21:44:43 -0300

    criacao da rota na api para exibir as musicas

---
+++ ajax/routes/visualizar.js
@@ -0,0 +1,15 @@
+const mongoose = require('../db/db')
+const Musica = require('../db/models/musica')
+
+module.exports = app => {
+
+    app.get('/visualizar', (req, res) => {
+
+        Musica.find({}, (err, docs) => {
+
+            res.send(docs)
+
+
+        })
+    })
+}
